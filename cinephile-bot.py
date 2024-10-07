9f528e8a636377ae3e1ff6b05a18d5306b33d376
Tyler Vo <54518684+tylerhv@users.noreply.github.com>
2024-10-07 19:51:03 -0400

    set up card distribution logic

--- cinephile-bot.py
+++ cinephile-bot.py
@@ -1,22 +1,21 @@
 import os
 import discord
 from discord import User
-
-actor_path = "reference/actors.txt"
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
+actor_path = "reference/actors.txt"
 cinephile_players = []
 last_actor_played = ""
 
 with open(actor_path, "r") as actors_file:
-    actors = actors_file.read()
+    actors = actors_file.read().split("\n")
 
-print(actors.split("\n"))
-    
+index = [i for i in range(len(actors))]
+
 
 class Player:
     def __init__(self, username):
@@ -40,5 +39,16 @@ async def on_message(message):
     if message.content.startswith("!Play"):
         last_actor_played = " ".join(message.content.split()[1:])
         print(last_actor_played)
+
+    if message.content.startswith("!Start"):
+        await message.channel.send(f"Distributing cards...")
+        cards_to_distribute = 6
+        for player in cinephile_players:
+            for i in range(cards_to_distribute):
+                card = random.choice(actors)
+                player.cards.append(card)
+                actors.remove(card)
+                  
+
 
 client.run(os.getenv("DISCORD_TOKEN"))
