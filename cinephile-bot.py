88ff507dc05b9824ebd060ec41ad06f2a55112ed
Tyler Vo <54518684+tylerhv@users.noreply.github.com>
2025-03-30 13:07:12 -0400

    refactor play command

--- cinephile-bot.py
+++ cinephile-bot.py
@@ -6,6 +6,7 @@ from utils.verify_state import verify_state
 from utils.verify_player import verify_player
 from utils.verify_actor import verify_actor
 from commands.join import join_game
+from commands.play import play_card
 import random
 
 intents = discord.Intents.default()
@@ -46,26 +47,8 @@ async def on_message(message):
         return
         
     if message.content.startswith("!play"):
-        state_check = verify_state(state, "cinephiles")
-        player_check = verify_player(message.author.name, cinephile_players[current_turn].username)
-        if state_check == False:
-            await message.channel.send(f"You cannot use that command right now!")
-            return None
-        if player_check == False:
-            await message.channel.send(f"It's not your turn right now!")
-            return None 
-        actor = " ".join(message.content.split()[1:])
-        current_player_index = player_index_reference.index(message.author.name)
-
-        actor_check = verify_actor(actor, cinephile_players[current_player_index].cards)
-
-        if actor_check == False:
-            await message.channel.send(f"Invalid actor name!")
-            return None
-
-        cinephile_players[current_player_index].cards.remove(actor)
-        last_actor_played = actor
-        await message.channel.send(f"Current Card: {last_actor_played}")
+        await play_card(message, state, cinephile_players, current_turn, player_index_reference)
+        return
 
     if message.content.startswith("!start"):
         check = verify_state(state, "main_menu")
